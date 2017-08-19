4e322b5da37d287001a7a2bbb861d392910e0e73
ssnaitak <2262457008@qq.com>
2017-08-19 17:07:28 +0800

    头部东西

---
+++ assets/js/index.js
@@ -0,0 +1,27 @@
+var a = document.querySelectorAll('.lunbo li');
+
+var len = a.length;
+
+var index = 0;
+a[index].style.display = 'block';
+var timer = setInterval(function() {
+    a[index].style.display = 'none';
+    index++;
+    if (index > len - 1) {
+        index = 0
+    }
+    a[index].style.display = 'block';
+}, 4000)
+
+var inp = document.querySelector('.input-one input');
+var b = document.querySelector('.xiala');
+console.log(b)
+inp.onfocus = function() {
+    b.style.display = "block";
+    this.value = "";
+
+}
+inp.onblur = function() {
+    b.style.display = "none";
+    this.value = "请输入区域、商圈或小区名开始找房";
+}
